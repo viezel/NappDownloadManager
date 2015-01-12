94c6deb0e99094c4d6c64ae9bf60344c27ed2598
Mads Møller <mm@napp.dk>
2015-01-12 22:24:02 +0100

    add a TiLog

---
+++ iphone/NappDebug.h
@@ -0,0 +1,13 @@
+//
+//  NappDebug.h
+//  NappDownloadManager
+//
+//  Created by Mads Møller on 12/1/15.
+//
+//
+
+#ifndef NappDownloadManager_NappDebug_h
+#define NappDownloadManager_NappDebug_h
+
+
+#endif
